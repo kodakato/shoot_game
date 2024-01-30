513c961f3eecc4778ff421217e1720dcaa8d8271
Koda <icekoda@protonmail.com>
2024-01-30 12:58:30 -0500

    Added player movement, and basic spawning of enemies

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 
+const PLAYER_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
+const ROTATION_SPEED: f32 = 2.0;
+
+// Enemy
+const ENEMY_SIZE: Vec2 = Vec2::new(20.0, 20.0);
+const ENEMY_SPAWN_SECONDS: u64 = 2;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Scoreboard { score: 0 })
         .add_systems(Startup, setup)
+        .add_systems(Update, player_movement)
+        .add_systems(Update, spawn_enemy)
         .run();
 }
 
@@ -17,14 +27,109 @@ struct Scoreboard {
     score: usize,
 }
 
+#[derive(Component)]
+struct Player {
+    speed: f32,
+    acceleration: f32,
+    velocity: Vec3,
+    rotation_velocity: f32,
+    rotation_acceleration: f32,
+}
+
+#[derive(Component)]
+struct Enemy;
+
+#[derive(Resource)]
+struct EnemySpawnTimer(Timer);
+
 fn setup(
-    mut Commands: Commands,
+    mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
     // Camera
     commands.spawn(Camera2dBundle::default());
 
-    // Shoot Sound
-    
+    // Spawn Player
+    let player_texture = asset_server.load("sprites/rocket.png");
+    commands.spawn((
+        SpriteBundle {
+            texture: player_texture,
+            ..default()
+        },
+        Player { 
+            speed: 100.0, 
+            acceleration: 100.0, 
+            velocity: Vec3::ZERO,
+            rotation_velocity: 0.0,
+            rotation_acceleration: 0.5,
+        },
+    ));
 
+    // Enemy spawn timer
+    commands.insert_resource(EnemySpawnTimer(Timer::new(Duration::from_secs(ENEMY_SPAWN_SECONDS), TimerMode::Repeating)));
+}
+
+fn player_movement(
+    mut player: Query<(&mut Transform, &mut Player)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, mut player) in player.iter_mut() {
+        
+        // Rotation acceleration and deceleration
+        if input.pressed(KeyCode::Left) {
+            player.rotation_velocity += player.rotation_acceleration * time.delta_seconds();
+        } else if input.pressed(KeyCode::Right) {
+            player.rotation_velocity -= player.rotation_acceleration * time.delta_seconds();;
+        }
+        // Clamp to max rotation speed
+        player.rotation_velocity = player.rotation_velocity.clamp(-ROTATION_SPEED, ROTATION_SPEED);
+        // Apply rotation
+        transform.rotate(Quat::from_rotation_z(player.rotation_velocity * time.delta_seconds()));
+
+
+
+        // Get forward vector
+        let forward = transform.rotation.mul_vec3(Vec3::Y);
+        let acceleration = player.acceleration;
+        // Acceleration        
+        if input.pressed(KeyCode::Down) {
+            player.velocity += forward * acceleration * time.delta_seconds();
+        } else { // Deceleration
+            let decel = 0.1 * time.delta_seconds();
+            let velocity = player.velocity;
+            if player.velocity.length() > decel {
+                player.velocity -= velocity.normalize() * decel;
+            } else {
+                player.velocity = Vec3::ZERO;
+            }
+        }
+
+        player.velocity = player.velocity.clamp_length_max(player.speed); // Clamp to max speed
+
+        // Apply velocity to position
+        transform.translation += player.velocity * time.delta_seconds();
+
+    }
+}
+
+fn spawn_enemy(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut spawn_timer: ResMut<EnemySpawnTimer>,
+
+) {
+    if spawn_timer.0.tick(time.delta()).just_finished() {
+        commands.spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(1.0, 0.0, 0.0),
+                    custom_size: Some(ENEMY_SIZE),
+                    ..default()
+                },
+                ..default()
+            },
+            Enemy,
+        ));
+    }
 }
