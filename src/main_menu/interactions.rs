ea7359b0c1560e4e22f6e510a7a13eac104bfcfe
Koda <kodakato@protonmail.com>
2024-02-25 23:10:58 -0500

    added quit button stuff

--- src/main_menu/interactions.rs
+++ src/main_menu/interactions.rs
@@ -3,26 +3,44 @@ use bevy::prelude::*;
 use crate::main_menu::components::{PlayButton, QuitButton};
 
 pub fn interact_with_play_button(
-    mut button_query: Query<(&Interaction, &mut BackgroundColor),(Changed<Interaction>, With<PlayButton>)>
+    mut button_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<PlayButton>),
+    >,
 ) {
     if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
         match *interaction {
             Interaction::Pressed => {
-                println!("Play button clicked!");
                 *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 1.0));
             }
             Interaction::Hovered => {
-                println!("Play button hovered!");
                 *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 0.5));
             }
             Interaction::None => {
-                println!("Play button none!");
                 *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 0.0));
             }
         }
     }
 }
 
-pub fn interact_with_quit_button() {
-
+pub fn interact_with_quit_button(
+    mut button_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<QuitButton>),
+    >,
+) {
+    if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
+        match *interaction {
+            Interaction::Pressed => {
+                *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 1.0));
+            }
+            Interaction::Hovered => {
+                *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 0.5));
+            }
+            Interaction::None => {
+                *background_color = BackgroundColor(Color::rgb(0.0, 0.0, 0.0));
+            }
+        }
+    }
 }
+
--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,21 +1,23 @@
-use bevy::prelude::*;
 use crate::AppState;
+use bevy::prelude::*;
 
 pub struct MainMenuPlugin;
 
-pub mod layout;
 pub mod components;
 pub mod interactions;
+pub mod layout;
 
-use layout::*;
 use interactions::*;
+use layout::*;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
-        .add_systems(OnExit(AppState::MainMenu), despawn_main_menu)
-        .add_systems(Update, (
-            interact_with_play_button,
-        ).run_if(in_state(AppState::MainMenu)));
+            .add_systems(OnExit(AppState::MainMenu), despawn_main_menu)
+            .add_systems(
+                Update,
+                (interact_with_play_button, interact_with_quit_button)
+                    .run_if(in_state(AppState::MainMenu)),
+            );
     }
 }
